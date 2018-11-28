e2e55c3d80b37fca24ee8e6635d5608b4f085c6e
Vasilyi Lapshinkin <vaseeeeek53@gmail.com>
2018-11-28 12:51:46 +0300

    slaider

--- js/main.js
+++ js/main.js
@@ -1,21 +1,73 @@
-$().ready(function(){
+$().ready(function () {
 
-   let activeClass = "menu-list__item_active";
+  let activeClass = "menu-list__item_active";
 
-   let btn = $('.menu-list__title');
+  let btn = $('.menu-list__title');
 
-   btn.click(function(event){
-     event.preventDefault();
+  btn.click(function (event) {
+    event.preventDefault();
+    var parent = $(this).parent();
+
+    if (parent.hasClass(activeClass)) {
+      parent.remove(activeClass)
+    } else {
+      btn.parent().removeClass(activeClass);
+      parent.addClass(activeClass);
+    }
+
+  });
+
+  let currentSlide = 0;
+  let arrayImgList = ['../img/darkburger.png', '../slide/burger_sandwich_PNG4124.png', '../slide/Chiken burger.png', '../slide/NYC-Diner-Bacon-Cheeseburger.png', '../slide/Spicy_Shrimp_Burger.png']
+  let objectList = [
+    {
+      src: '../img/darkburger.png',
+      burgerName: 'Dark beef burger',
+      title: 'Ваш старый добрый знакомый, с рубленым бифштексом из 100% говядины, тремя кусочками нежнейшего сыра эмменталь, двумя помидорами, луком, салатом и соусом гриль теперь с добавлением экзотического соуса чураско.',
+    },
+    {
+      src: '../slide/burger_sandwich_PNG4124.png',
+      burgerName: 'SANDWICH',
+      title: 'Ваш старый добрый знакомый , с рубленым бифштексом из 100% говядины, луком, салатом и соусом гриль теперь с добавлением экзотического соуса чураско.',
+    },
+    {
+      src: '../slide/Chiken burger.png',
+      burgerName: 'Chiken',
+      title: 'Ваш старый добрый знакомый, с рубленым бифштексом из 100% КУРИЦЫ, тремя кусочками нежнейшего сыра эмменталь, двумя помидорами, луком, салатом и соусом гриль теперь с добавлением экзотического соуса чураско.',
+    },
+    {
+      src: '../slide/NYC-Diner-Bacon-Cheeseburger.png',
+      burgerName: 'Bacon Cheeseburger',
+      title: 'Ваш старый добрый знакомый Bacon-Cheeseburger, с беконом, тремя кусочками нежнейшего сыра эмменталь, двумя помидорами, луком, салатом и соусом гриль теперь с добавлением экзотического соуса чураско.',
+    },
+    {
+      src: '../slide/Spicy_Shrimp_Burger.png',
+      burgerName: 'Shrimp Burger',
+      title: 'Ваш старый добрый знакомый Shrimp Burger,c мясом, двумя помидорами, луком, салатом и соусом гриль теперь с добавлением экзотического соуса чураско.',
+    }
+    
+  ]
 
 
-     var parent = $(this).parent();
+  $('#moveBack').on('click', function () {
+    console.log('backward');
+    if (currentSlide > 0) {
+      currentSlide--;
+      let current = objectList[currentSlide];
+      $('.burger-img--icon').attr("src", current.src);
+      $('.title--burger').text(current.burgerName);
+      $('.burger-description__recipe').text(current.title);
+    }
+  });
 
-     if (parent.hasClass(activeClass)){
-       parent.remove(activeClass)
-     }else{
-       btn.parent().removeClass(activeClass);
-       parent.addClass(activeClass);
-     }
-
-   });
+  $('#moveForward').on('click', function () {
+    console.log('forwardward');
+    if (currentSlide < objectList.length - 1) {
+      currentSlide++;
+      let current = objectList[currentSlide];
+      $('.burger-img--icon').attr("src", current.src);
+      $('.title--burger').text(current.burgerName);
+      $('.burger-description__recipe').text(current.title);
+    }
+  });
 });
